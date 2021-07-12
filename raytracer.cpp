dcacb5b9d01fe281a3db5043c7b6958faa3d6591
lucac123 <63089808+lucac123@users.noreply.github.com>
2021-07-12 17:07:09 -0500

    Utilized stb_image_write to write final image to JPG

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,4 +1,7 @@
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <iostream>
+#include <cstring>
+#include <stb_image_write.h>
 
 // ppm image renderer, writes to stdout, must pipe output to file to view
 
@@ -7,11 +10,16 @@ int main() {
     const int image_width = 256;
     const int image_height = 256;
 
-    std::cout << "P3" << std::endl << image_width << ' ' << image_height << std::endl << 255 << std::endl;
+    unsigned char data[image_width*image_height*3];
 
-    for (int i = 0; i < 256; i++) {
-        for (int j = 0; j < 256; j++) {
-            std::cout << i << '\t' << j << '\t' << (i+j)/2 << std::endl;
+    for (int i = 0; i < image_height; i++) {
+        for (int j = 0; j < image_width; j++) {
+            int base_adr = (image_width*i+j)*3;
+            data[base_adr] = (unsigned char)i;
+            data[base_adr+1] = (unsigned char) j;
+            data[base_adr+2] = (unsigned char) 125;
         }
     }
+
+    stbi_write_jpg("out.jpg", image_width, image_height, 3, data, 100);
 }
