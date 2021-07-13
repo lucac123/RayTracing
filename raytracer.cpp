4be078473e771e13a7b99b0dc49a2d9093eb1bcb
lucac123 <63089808+lucac123@users.noreply.github.com>
2021-07-13 01:45:37 -0500

    Added vec3 class

--- raytracer.cpp
+++ raytracer.cpp
@@ -6,22 +6,28 @@
 // ppm image renderer, writes to stdout, must pipe output to file to view
 
 int main() {
-    //Image
-    const int image_width = 256;
-    const int image_height = 256;
+	//Image
+	const int image_width = 256;
+	const int image_height = 256;
 
-    unsigned char data[image_width*image_height*3];
+	unsigned char data[image_width*image_height*3];
 
-    for (int i = 0; i < image_height; i++) {
-        std::cout << "\rRendering Line " << i+1 << " of " << image_height << std::flush;
-        for (int j = 0; j < image_width; j++) {
-            int base_adr = (image_width*i+j)*3;
-            data[base_adr] = (unsigned char)i;
-            data[base_adr+1] = (unsigned char) j;
-            data[base_adr+2] = (unsigned char) 125;
-        }
-    }
-    std::cout << "\nRender Complete\n";
+	for (int i = 0; i < image_height; i++) {
+		std::cout << "\rRendering Line " << i+1 << " of " << image_height << std::flush;
+		for (int j = 0; j < image_width; j++) {
+			int base_adr = (image_width*i+j)*3;
 
-    stbi_write_jpg("out.jpg", image_width, image_height, 3, data, 100);
+			int r = (int) ((double)(image_height-i)/image_height * 255.999);
+			int g = (int) ((double)(image_width-j)/image_width * 255.999);
+			int b = (int) ((double)(j)/image_width * 255.999);
+
+
+			data[base_adr] = (unsigned char)r;
+			data[base_adr+1] = (unsigned char) 0;
+			data[base_adr+2] = (unsigned char) b;
+		}
+	}
+	std::cout << "\nRender Complete\n";
+
+	stbi_write_jpg("out.jpg", image_width, image_height, 3, data, 100);
 }
---
+++ vec3.h
@@ -0,0 +1,64 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <iostream>
+#include <cmath>
+
+using std::sqrt;
+
+class vec3 {
+private:
+	//Vector Values
+	float e[3];
+
+public:
+	//Constructor
+	vec3() : e{0, 0, 0} {}
+	vec3(float e0, float e1, float e2) : e{e0, e1, e2} {}
+
+	//Getters
+	float x() const { return e[0] }
+	float y() const { return e[1] }
+	float z() const { return e[2] }
+
+	//Operator Overloading
+
+		//Negative
+	vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]) }
+		//Constant Value Accessing
+	float operator[](int i) const { return e[i] }
+		//Variable Value Accessing
+	float& operator[](int i) { return e{i} }
+
+	vec3& operator+=(const vec3& v) {
+		e[0] += v[0];
+		e[1] += v[1];
+		e[2] += v[2];
+		return *this;
+	}
+
+	vec3& operator*=(const float t) {
+		e[0] *= t;
+		e[1] *= t;
+		e[2] *= t;
+		return *this;
+	}
+
+	vec3& operator/=(const float t) {
+		return *this *= 1/t;
+	}
+
+	float length() const {
+		return sqrt(length_squared());
+	}
+
+	float length_squared() const {
+		return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+	}
+};
+
+//Type Aliases
+using point3 = vec3; //3D point
+using color = vec3; //RGB color
+
+#endif
