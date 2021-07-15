50f1fd12f31efcf546187381185cc88a09673a55
lucac123 <63089808+lucac123@users.noreply.github.com>
2021-07-14 21:39:02 -0500

    Added vec3 class

--- vec3.h
+++ vec3.h
@@ -17,18 +17,18 @@ public:
 	vec3(float e0, float e1, float e2) : e{e0, e1, e2} {}
 
 	//Getters
-	float x() const { return e[0] }
-	float y() const { return e[1] }
-	float z() const { return e[2] }
+	float x() const { return e[0]; }
+	float y() const { return e[1]; }
+	float z() const { return e[2]; }
 
 	//Operator Overloading
 
 		//Negative
-	vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]) }
+	vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
 		//Constant Value Accessing
-	float operator[](int i) const { return e[i] }
+	float operator[](int i) const { return e[i]; }
 		//Variable Value Accessing
-	float& operator[](int i) { return e{i} }
+	float& operator[](int i) { return e[i]; }
 
 	vec3& operator+=(const vec3& v) {
 		e[0] += v[0];
@@ -56,6 +56,50 @@ public:
 		return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
 	}
 };
+
+//Utility Functions
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+	return out << v[0] << ' ' << v[1] << ' ' << v	[2];
+}
+
+inline vec3 operator+(const vec3& v, const vec3& u) {
+	return vec3(v[0] + u[0], v[1] + u[1], v[2] + u[2]);
+}
+
+inline vec3 operator-(const vec3& v, const vec3& u) {
+	return vec3(v[0] - u[0], v[1] - u[1], v[2] - u[2]);
+}
+
+		//Hadamard Product
+inline vec3 operator*(const vec3& v, const vec3& u) {
+	return vec3(v[0]*u[0], v[1]*u[1], v[2]*u[2]);
+}
+
+inline vec3 operator*(const vec3& v, const float t) {
+	return vec3(v[0]*t, v[1]*t, v[2]*t);
+}
+
+inline vec3 operator*(const float t, const vec3& v) {
+	return v * t;
+}
+
+inline vec3 operator/(const vec3& v, const float t) {
+	return v * (1/t);
+}
+
+inline float dot(const vec3& v, const vec3& u) {
+	return v[0]*u[0] + v[1]*u[1] + v[2]*u[2];
+}
+
+inline vec3 cross(const vec3& v, const vec3& u) {
+	return vec3(v[1]*u[2] - v[2]*u[1], 
+				v[2]*u[0] - v[0]*u[2],
+				v[0]*u[1] - v[1]*u[0]);
+}
+
+inline vec3 unit_vector(vec3 v) {
+	return v / v.length();
+}
 
 //Type Aliases
 using point3 = vec3; //3D point
